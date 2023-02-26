9957c2a0cf6e794faa1f1d425166e3d181263a2b
RichDoherty <richdoherty7@gmail.com>
2023-02-26 11:08:19 -0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/sorting-algorithms',
   plugins: [solidPlugin()],
   server: {
     port: 3000,
